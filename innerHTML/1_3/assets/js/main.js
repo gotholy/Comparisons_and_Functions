28f4bd9f2855a288e6f35bf492dd7de61d6c83c0
Andre Wolf <Andre77242@hotmail.de>
2023-09-12 16:12:42 +0200

    innerHTML1_3 flex with .style fixed

--- innerHTML/1_3/assets/js/main.js
+++ innerHTML/1_3/assets/js/main.js
@@ -5,7 +5,7 @@ galleryContainer.innerHTML = `
 <figure><img src='https://picsum.photos/id/1/300/300' alt='pic1'>Fig 2</figure>
 <figure><img src='https://picsum.photos/id/1/300/300' alt='pic1'>Fig 3</figure>
 `;
-
+galleryContainer.style.display = "flex";
 /*(let gallery = document.getElementById("gallery");
 gallery.innerHTML =
   "<figure><img src='https://picsum.photos/id/1/300/300'/><p>Fig.1</p></figure><figure><img src='https://picsum.photos/id/1/300/300'/><p>Fig.2</p></figure><figure><img src='https://picsum.photos/id/1/300/300'/><p>Fig.3</p></figure>";*/
